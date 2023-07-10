e6e4f71b92022b641488ec1802c65a29fa7995b3
LighteningBolt13 <joywinpinto1@gmail.com>
2023-07-10 16:51:26 +0530

    Crop Production

---
+++ CropPredictionApi.py
@@ -0,0 +1,27 @@
+from flask import Flask , request,jsonify
+import numpy as np
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open("xgboostmodel2.sav",'rb'))
+df = pickle.load(open('columns.csv', 'rb'))
+
+@app.route("/",methods = ['POST'])
+def prediction():
+    data = request.get_json(force=True)
+    state_name = data['state']
+    season = data['season']
+    crop = data['crop']
+    area = float(data['area'])
+    df2 = pd.DataFrame(0,index = range(1), columns=df.columns)
+    df2['State_Name_'+state_name] = 1
+    df2['Season_'+season] = 1
+    df2['Crop_'+crop] = 1
+    df2['Area'] = area
+    print(df2.shape)
+    Production = model.predict(df2)
+    return (str(Production*area))
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
