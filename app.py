e77e009979d0f462618a1aa140f4eb67f08a1128
LighteningBolt13 <joywinpinto1@gmail.com>
2023-07-10 18:01:30 +0530

    Fixing the columns of the pandas dataframe

--- app.py
+++ app.py
@@ -6,8 +6,17 @@ from joblib import load
 
 app = Flask(__name__)
 model = load('xgboostmodel.joblib')
-df = pd.read_csv("crop_production.csv")
+data = pd.read_csv("crop_production.csv")
+df = pd.DataFrame(data)
+df = df.dropna()
+df = df.drop(['Production',"District_Name","Crop_Year"],axis = 1)
+df['State_Name'] = df['State_Name'].str.replace(" ","")
+df['Season'] = df['Season'].str.replace(" ","")
+df['Crop'] = df['Crop'].str.replace(" ","")
+df = pd.get_dummies(df, columns = ['State_Name', 'Season','Crop'])
+df = df.astype(float)
 
+print(df)
 @app.route("/",methods = ['POST'])
 def prediction():
     data = request.get_json(force=True)
