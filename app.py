fa8c8456f7381292af731d7c5f7621b1fa93bc57
LighteningBolt13 <joywinpinto1@gmail.com>
2023-07-12 14:01:40 +0530

    Chat model deployed?

--- app.py
+++ app.py
@@ -2,10 +2,62 @@ from flask import Flask , request,jsonify
 import numpy as np
 import pandas as pd
 import pickle
+import random
+from textblob import TextBlob
 from joblib import load
+from keras.preprocessing.text import Tokenizer
+import re
+import nltk
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from keras.utils.data_utils import pad_sequences
+
 
 app = Flask(__name__)
 model = load('xgboostmodel.joblib')
+chatmodel = load('chatbotmodel.joblib')
+tokenizer_obj = load('tokenizer.joblib')
+
+def clean_text(text):
+    all_reviews = []
+    pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    now_text = text[0]
+    now_text = now_text.lower()
+    now_text = pattern.sub('', now_text)
+    now_text = re.sub(r"[,.\"!@#$%^&*(){}?/;`~:<>+=-]", "", now_text)
+    now_text = re.sub(r"i'm", "i am", now_text)
+    now_text = re.sub(r"he's", "he is", now_text)
+    now_text = re.sub(r"she's", "she is", now_text)
+    now_text = re.sub(r"that's", "that is", now_text)        
+    now_text = re.sub(r"what's", "what is", now_text)
+    now_text = re.sub(r"where's", "where is", now_text) 
+    now_text = re.sub(r"\'ll", " will", now_text)  
+    now_text = re.sub(r"\'ve", " have"  , now_text)  
+    now_text = re.sub(r"\'re", " are", now_text)
+    now_text = re.sub(r"\'d", " would", now_text)
+    now_text = re.sub(r"\'ve", " have", now_text)
+    now_text = re.sub(r"won't", "will not", now_text)
+    now_text = re.sub(r"don't", "do not", now_text)
+    now_text = re.sub(r"did't", "did not", now_text)
+    now_text = re.sub(r"can't", "can not", now_text)
+    now_text = re.sub(r"it's", "it is", now_text)
+    now_text = re.sub(r"couldn't", "could not", now_text)
+    now_text = re.sub(r"have't", "have not", now_text)
+    tokens = word_tokenize(now_text)
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    words = [word for word in stripped if word.isalpha()]
+
+    PS = PorterStemmer()
+    words = [PS.stem(w) for w in words ]
+    words = ' '.join(words)
+    all_reviews.append(words)
+    return all_reviews
+
+
+
 
 @app.route("/",methods = ['POST'])
 def prediction():
@@ -185,6 +237,55 @@ def prediction():
     print(df2.shape)
     Production = model.predict(df2)
     return (str(Production*area))
+
+@app.route("/chat",methods = ['POST'])
+def chat():
+    data = request.get_json(force=True)
+    text = data['text']
+    dict = {'predict': 0,
+ 'recommend': 1,
+ 'goodbye': 2,
+ 'name': 3,
+ 'greeting': 4,
+ 'weather': 5,
+ 'stores': 6,
+ 'crops': 7,
+ 'friends': 8,
+ 'delete': 9,
+ 'notfound': 10,
+ 'communication': 11}
+    dictionary2 = {'predict': ['You can go to our homepage and click on get started 🤝'],
+ 'recommend': ['Congrats! You accessed one of to-be-developed features. I am sincerely sorry to tell that the feature is not yet available',
+  'Features is in development stage. Inconvience regretted'],
+ 'goodbye': ['Bye', 'take care'],
+ 'name': ['My name is farm sensei. Developers named me that',
+  'I am farm sensei. Ready to be at your service'],
+ 'greeting': ['Hi there. I am Farm Sensei!. Ready to help', 'Hello', 'Hi :)'],
+ 'weather': ['Go to homepage and access weather forecast for our amazing features.'],
+ 'stores': ['Farm sensei to the rescue! You can access stores from the homepage!',
+  'To know the stores near you can use our stores feature in the homepage'],
+ 'crops': ['Farm sensei to the rescue! You can access crops from the major crops in the homepage!',
+  'To know the crops grown near you can use our major crops feature in the homepage 🚀'],
+ 'friends': ['Farm sensei to the rescue! Click on your profile on the toppage! There you go you should see a friends bar on the right!!'],
+ 'delete': ['Extremely sorry to hear that! We will improve in the future. If you are unhappy with our service delete the account is present in the profile accessed by clicking on the top of your page'],
+ 'notfound': ['Farm Sensei encourage to fill the form in the profile page. Profile is accessed by clicking on the top of the page. Then you must be able to see the farmers near you!!'],
+ 'communication': ['Farm Sensei encourage to go to profile page. Then you must be able to see the farmers near your area. Send them a friend request. And after accepting you will be able to see their phone number']}
+    text = str(TextBlob(text).correct())
+    print(text)
+    test_lines = clean_text([text])
+    test_sequences = tokenizer_obj.texts_to_sequences(test_lines)
+    test_review_pad = pad_sequences(test_sequences, maxlen=15, padding='post')
+    print(test_review_pad)
+    
+    pred = chatmodel.predict([test_review_pad])
+    pred*=100
+    pred[0] = np.array(pred[0])
+    print(pred)
+    i = np.argmax(pred[0])
+    inverse_dict = {value: key for key, value in dict.items()}
+    print(inverse_dict[i])
+    ourResult = random.choice(dictionary2[inverse_dict[i]])
+    return (ourResult)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
